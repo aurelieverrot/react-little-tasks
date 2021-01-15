32c349535fd2eb7f545b42df0d3dce3ffd281a84
Aurelie Verrot <aurelie+github@verrot.fr>
2021-01-15 11:23:30 -0800

    get list of tasks from axios

--- src/components/TasksContainer.js
+++ src/components/TasksContainer.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class TasksContainer extends Component {
+    state = {
+        tasks: []
+    }
+
+    getTasks() {
+        axios.get('/tasks')
+            .then(response => {
+                this.setState({
+                    tasks: response.data
+                })
+            })
+            .catch(error => console.log(error))
+    }
+
+    componentDidMount() {
+        this.getTasks()
+    }
+
     render() {
         return (
             <div>
                 <div className="inputContainer">
                     <input type="text" className="taskInput" placeholder="Add a new task" maxLength="50"/>
+                    <button>I need a button here</button>
                 </div>
                 <div className="tasksWrapper">
                     <ul className="taskList">
-
+                        {this.state.tasks.map( task => {
+                            return(
+                                <li className="task" todo={task} key={task.id}>
+                                    <input className="taskCheckbox" type="checkbox" />
+                                    <label className="taskLabel">{task.title}</label>
+                                    <button className="doneTaskBtn">x</button>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
             </div>
